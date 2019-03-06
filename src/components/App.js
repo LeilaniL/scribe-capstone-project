535ee015d1407c2329a761259f2d90b13c47e937
Leilani Leach <leilanileach@yahoo.com>
2019-03-06 14:28:41 -0800

    add bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Transcript</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
