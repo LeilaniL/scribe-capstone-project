0f4d3d755e1100538865bbadfddc0d79908f0ca7
Leilani Leach <leilanileach@yahoo.com>
2019-03-07 18:32:14 -0800

    refactoring

--- src/components/TranscriptionBody.jsx
+++ src/components/TranscriptionBody.jsx
@@ -1,7 +1,49 @@
 import React, { Component } from 'react';
-import getTranscript from '../getTranscript';
-import encodeAudio from '../encodeAudio';
+// import getTranscript from '../getTranscript';
+// import encodeAudio from '../getTranscript';
+import apikey from '../apikey';
 
+// Encode audio file when user selects one
+var encoded;
+function encodeAudio(event) {
+    // grab user-selected file
+    let reader = new FileReader();
+    const file = event.target.files[0];
+    // convert file to string including base64
+    reader.readAsDataURL(file);
+    reader.onload = (file) => {
+        // separate base64 from result string
+        encoded = reader.result.split(",");
+        console.log("I am result split: ", encoded[1])
+    }
+}
+
+// Send encoded audio data to Google API when button clicked
+
+function getTranscript(encoded) {
+    console.log("I am encoded inside getTranscript", encoded);
+    // add Google Cloud project API key to src/apikey.js
+    let url = "https://speech.googleapis.com/v1/speech:recognize?key=" + apikey;
+    let config = {
+        "encoding": "LINEAR16",
+        "sampleRateHertz": 16000,
+        "languageCode": "en-US"
+    }
+    let audio = {
+        "content": encoded
+        // audioData = base64 encoded file (can get from https://www.giftofspeed.com/base64-encoder/)
+    }
+    let requestBody = {
+        "audio": audio,
+        "config": config,
+    }
+
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(requestBody)
+    }).then(res => res.json()).then(response => console.log('Success!', JSON.stringify(response))).catch(error => console.error('Error: ', error));
+    alert("I was clicked");
+}
 var transcriptStyles = {
     backgroundColor: 'lightgray',
     minWidth: '33%',
@@ -11,17 +53,9 @@ var transcriptStyles = {
 }
 
 class TranscriptionBody extends Component {
-    // state = {  }
-    audioData(event) {
-        console.log("audio data called");
-        console.log(event.target.files[0]);
-        // console.log(event.target.result);
-        let reader = new FileReader();
-        const file = event.target.files[0]
-        console.log("readAsDataURL :", reader.readAsDataURL(file));
-        reader.onload = (file) => {
-            console.log(reader.result)
-        }
+    state = {
+        audioWasEncoded: "false",
+        base64Data: ""
     }
     render() {
         return (
@@ -31,7 +65,7 @@ class TranscriptionBody extends Component {
                 </div>
                 {/* <button onClick={getTranscript} id="test" className="btn-lg btn-danger">Get Transcript</button> */}
                 <input type="file" id="audioUpload" onChange={(event) => { encodeAudio(event) }}></input>
-                <button className="green-button"> Transcribe</button>
+                <button className="green-button" onClick={getTranscript}> Transcribe</button>
                 <br />
             </div>
         );
--- src/getTranscript.js
+++ src/getTranscript.js
@@ -1,26 +1,28 @@
-import apikey from './apikey';
+// import apikey from './apikey';
+// import encodeAudio from './encodeAudio';
 
-function getTranscript() {
-    // add Google Cloud project API key to src/apikey.js
-    let url = "https://speech.googleapis.com/v1/speech:recognize?key=" + apikey;
-    let config = {
-        "encoding": "LINEAR16",
-        "sampleRateHertz": 16000,
-        "languageCode": "en-US"
-    }
-    let audio = {
-        // "content": audioData
-        // audioData = base64 encoded file (can get from https://www.giftofspeed.com/base64-encoder/)
-    }
-    let requestBody = {
-        "audio": audio,
-        "config": config,
-    }
+// function getTranscript(encoded) {
+//     console.log("I am encoded inside getTranscript", encoded);
+//     // add Google Cloud project API key to src/apikey.js
+//     let url = "https://speech.googleapis.com/v1/speech:recognize?key=" + apikey;
+//     let config = {
+//         "encoding": "LINEAR16",
+//         "sampleRateHertz": 16000,
+//         "languageCode": "en-US"
+//     }
+//     let audio = {
+//         // "content": audioData
+//         // audioData = base64 encoded file (can get from https://www.giftofspeed.com/base64-encoder/)
+//     }
+//     let requestBody = {
+//         "audio": audio,
+//         "config": config,
+//     }
 
-    fetch(url, {
-        method: "POST",
-        body: JSON.stringify(requestBody)
-    }).then(res => res.json()).then(response => console.log('Success!', JSON.stringify(response))).catch(error => console.error('Error: ', error));
-    alert("I was clicked");
-}
-export default getTranscript;
+//     // fetch(url, {
+//     //     method: "POST",
+//     //     body: JSON.stringify(requestBody)
+//     // }).then(res => res.json()).then(response => console.log('Success!', JSON.stringify(response))).catch(error => console.error('Error: ', error));
+//     // alert("I was clicked");
+// }
+// export default getTranscript;
